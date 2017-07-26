b92e56ffe954c9e628699bd591711fdae9f907a8
Hasanat Kazmi <hasanatkazmi@gmail.com>
2017-07-26 02:49:59 -0700

    minor fix

--- app/test-smoke.sh
+++ app/test-smoke.sh
@@ -12,5 +12,5 @@ echo "It should print returned json with key 'foo' and value 'xyz'"
 curl -X POST -H "Content-Type: application/json" -X POST -d '{"foo":"xyz"}'  http://localhost:80/
 echo
 echo "If server is running with SERVER_MODE set as truthy value, then it should be greped here:"
-# docker-compose has standard container naming so this container should be named plangridassignment_app_1
-docker exec plangridassignment_app_1 cat /app/plangrid.log | grep xyz
+# docker-compose has standard container naming so this container should be named dockerizedflaskdemo_app_1
+docker exec dockerizedflaskdemo_app_1 cat /app/plangrid.log | grep xyz
